06320edf059a64188a102ad2b7f0c6619416ecd2
abdiabbaz <abs001@edu.zealand.dk>
2024-12-26 15:51:15 +0100

    Added a 'Add Task' feature and restructured some functions

--- javascript/app.js
+++ javascript/app.js
@@ -76,16 +76,6 @@ const app = Vue.createApp({
                     console.error("Failed to toggle completion", error);
                 });
         },
-        formatDateToInput(date) {
-            if (!date) return ""; 
-            const d = new Date(date);
-            const year = d.getFullYear();
-            const month = String(d.getMonth() + 1).padStart(2, "0");
-            const day = String(d.getDate()).padStart(2, "0");
-            const hours = String(d.getHours()).padStart(2, "0");
-            const minutes = String(d.getMinutes()).padStart(2, "0");
-            return `${year}-${month}-${day}T${hours}:${minutes}`;
-          },
           getCompletionClass(isCompleted) {
             return isCompleted ? 'table-success' : 'table-danger';
         },
